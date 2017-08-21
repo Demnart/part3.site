bca434440f3df2c9cb8da8edf570ae68f012de46
Genjo <insidia93@gmail.com>
2017-08-21 22:20:13 +0300

    Create reference for products

--- views/category/index.php
+++ views/category/index.php
@@ -4,6 +4,7 @@
 
 use app\components\MenuWidget;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 
 ?>
@@ -121,11 +122,10 @@ use yii\helpers\Html;
                                 <div class="single-products">
                                     <div class="productinfo text-center">
                                         <?= Html::img("@web/images/products/{$hit->img}",['alt'=>$hit->name])?>
-                                        <h2><?= $hit->price;?></h2>
-                                        <p><?= $hit->name;?></p>
+                                        <h2>$ <?= $hit->price;?></h2>
+                                        <p><a href="<?= Url::to(['product/view','id'=>$hit->id])?>"><?= $hit->name;?></a></p>
                                         <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                     </div>
-<!--
                                     <?php if ($hit->new):?>
                                         <?= Html::img("@web/images/home/new.png",['alt'=>'Новинка','class'=>'new'])?>
                                     <?php endif;?>
--- views/category/view.php
+++ views/category/view.php
@@ -64,10 +64,10 @@ use yii\helpers\Html;
                                 <div class="productinfo text-center">
                                     <?= Html::img("@web/images/products/{$product->img}")?>
                                     <h2>$<?= $product->price?></h2>
-                                    <p><?= $product->name?></p>
+                                    <p><a href="<?= \yii\helpers\Url::to(['product/view','id'=>$product->id])?>"><?= $product->name?></a></p>
                                     <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                 </div>
-<!--                                                              <?php if ($product->new):?>
+                             <?php if ($product->new):?>
                                     <?= Html::img("@web/images/home/new.png",['alt'=>'Новинка','class'=>'new'])?>
                                 <?php endif;?>
                                 <?php if ($product->sale):?>
@@ -88,7 +88,7 @@ use yii\helpers\Html;
                                 <div class="clearfix"></div>
                                 <?php endif;?>
                             <?php endforeach;?>
-                        <div class="clearfix"></div>
+                       <div class="clearfix"></div>
                         <?php
                         echo \yii\widgets\LinkPager::widget(['pagination'=>$pages,]);
                         ?>
